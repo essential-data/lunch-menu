23654816b81ad95d17c5d03bd843a503b0a5aaf8
Michal Kováč <kovac@essential-data.sk>
2016-04-14 11:14:19 +0200

    Fixed Ferdinand to index only current day

--- src/main/scala/sk/essentialdata/lunchmenu/restaurants/Ferdinand.scala
+++ src/main/scala/sk/essentialdata/lunchmenu/restaurants/Ferdinand.scala
@@ -1,9 +1,9 @@
 package sk.essentialdata.lunchmenu.restaurants
 
-import net.ruippeixotog.scalascraper.model.{Document, Element}
-import sk.essentialdata.lunchmenu.{Dish, Restaurant, SimpleDish}
-import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.model.Document
+import sk.essentialdata.lunchmenu.{Dish, Restaurant, SimpleDish}
 
 /**
   * @author miso
@@ -12,6 +12,6 @@ case object Ferdinand extends Restaurant {
   def url: String = "http://www.papanica.sk/sk/denne.php?id=4445&kraj=1"
 
   override def parse(doc: Document): Seq[Dish] = {
-    doc >> texts("td h3.text") map SimpleDish toSeq
+    doc >> element("#right table") >> texts("td h3.text") map SimpleDish toSeq
   }
 }
